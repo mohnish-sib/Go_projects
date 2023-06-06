45801a7de5c8c43c75b851349e7d9f547fc69f62
mohnish-sib <mohnish.lokhande@sendinblue.com>
2023-06-06 16:52:04 +0530

    put

--- main.go
+++ main.go
@@ -70,6 +70,31 @@ func createMovie(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(movie)
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request){
+	//set json content type
+	w.Header().Set("Content-Type","application/json")
+
+	// params
+	params:=mux.Vars(r)
+
+	//loop over the movies, range
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies =append(movies[:index],movies[index+1:]...)
+			var movie Movie
+			_ =json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID=params["id"]
+			movies=append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+
+	//delete the movie with the i.d that you have sent
+	//add a new movie, with updated data using same id
+	// not an ideal method, should not do in real projects
+}
+
 func main(){
 	r:=mux.NewRouter()
  
@@ -79,7 +104,7 @@ func main(){
 	r.HandleFunc("/movies",getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
 	r.HandleFunc("/movies",createMovie).Methods("POST")
-	// r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}",deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at 8080\n")
